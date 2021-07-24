2ee87f7f5022ae8c9c1d94d06c29204ed9fdfd02
malli <malli@10.0.0.165>
2021-07-24 22:34:00 +0530

    This is my first project

---
+++ src/main/java/com/javaspring/testservice/SpringDemoApplication.java
@@ -0,0 +1,20 @@
+package com.javaspring.testservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDemoApplication {
+
+	@GetMapping("/mymessage")
+	public String getMessage() {
+		return "This is my first message";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+}
